eeb954daee24f6d74048e8891e612d3a54df1433
MikeDmC <micheleilpro@libero.it>
2024-10-29 03:47:16 +0100

    creato l'enum per le categorie

---
+++ src/main/java/it/mikedmc/spring_eshop/model/enums/Category.java
@@ -0,0 +1,16 @@
+package it.mikedmc.spring_eshop.model.enums;
+
+public enum Category {
+    MENS_DRESSES,
+    WOMENS_DRESSES,
+    BABYS_DRESSES,
+    SHIRTS,
+    JEANS,
+    SWIMWEAR,
+    SLEEPWEAR,
+    SPORTSWEAR,
+    JUMPSUITS,
+    BLAZERS,
+    JACKETS,
+    SHOES
+}
