03bced0390eb1e67729fcff2e885001b9ef017ba
MikeDmC <micheleilpro@libero.it>
2024-11-02 04:51:46 +0100

    creata una classe che rappresenterà un prodotto selezionato nel carrello.

---
+++ src/main/java/it/mikedmc/spring_eshop/model/SelectedProduct.java
@@ -0,0 +1,31 @@
+package it.mikedmc.spring_eshop.model;
+
+public class SelectedProduct {
+    private Product product;
+    private Color color;
+    private Size size;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+}
